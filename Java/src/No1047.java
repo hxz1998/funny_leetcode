a1f565a1672fef73cefbd3b77181d1e30eb03cb1
Monkey <1466947023@qq.com>
2021-03-13 21:14:40 +0800

    写出了更好的算法

--- Java/src/No1047.java
+++ Java/src/No1047.java
@@ -7,7 +7,27 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class No1047 {
+
     public String removeDuplicates(String S) {
+        // 使用 StringBuilder 来模拟栈，
+        // 其提供的 charAt() 函数和 deleteCharAt 正好能满足需求
+        StringBuilder ans = new StringBuilder();
+        int top = -1;  // 用来记录栈顶位置
+        for (char c : S.toCharArray()) {
+            if (top >= 0 && ans.charAt(top) == c) {
+                // 如果栈 ans 不为空（top >= 0)，而且栈顶元素和当前读进来的字符一样
+                // 那么就删除掉栈顶元素，再把栈顶游标下移
+                ans.deleteCharAt(top);
+                --top;
+            } else {
+                // 否则的话，直接把字符添加到末尾，然后上移游标
+                ans.append(c);
+                ++top;
+            }
+        }
+        return ans.toString();
+    }
+/*    public String removeDuplicates(String S) {
         // 如果就一个字符，那么不用检查了，直接返回。
         if (S.length() == 1) return S;
         StringBuilder ans = new StringBuilder();
@@ -30,7 +50,7 @@ public class No1047 {
         // 构建返回值
         while (iter.hasNext()) ans.append(iter.next());
         return ans.toString();
-    }
+    }*/
 
     public static void main(String[] args) {
         No1047 no1047 = new No1047();
--- Java/src/No1441.java
+++ Java/src/No1441.java
@@ -13,17 +13,36 @@ public class No1441 {
 
     public List<String> buildArray(int[] target, int n) {
         List<String> ans = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
+        // 直接逐个检查 target 与当前 num 是否匹配
+        // 如果匹配呢，就直接 push 一个，然后游标 ++
+        // 如果不匹配，那么就 push + pop，然后游标不动
         for (int num = 1, index = 0; num <= n && index < target.length; ++num) {
-            stack.add(num);
-            ans.add(PUSH);
-            if (target[index] != stack.peek()) {
-                stack.pop();
+            if (num == target[index]) {
+                ans.add(PUSH);
+                ++index;
+            } else {
+                ans.add(PUSH);
                 ans.add(POP);
-            } else index++;
+            }
         }
         return ans;
     }
+/*    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        // num是产生的数字，index 是 target 数组的下标
+        for (int num = 1, index = 0; num <= n && index < target.length; ++num) {
+            // 不管是否符合要求，先把数和操作放进来
+            stack.add(num);
+            ans.add(PUSH);
+            // 检查一下是否符合要求，如果不符合，那么就把栈顶元素弹出来，然后再加入Pop操作
+            if (target[index] != stack.peek()) {
+                stack.pop();
+                ans.add(POP);
+            } else index++; // 符合要求的话，就把游标往右移动一个
+        }
+        return ans;
+    }*/
 
     public static void main(String[] args) {
         int[] target = new int[]{2, 3, 4};
