523fed67038c7d05a625855f92f25d44fe9b699c
Monkey <1466947023@qq.com>
2021-12-16 22:31:37 +0800

    同步源码

--- CPP/No748.cc
+++ CPP/No748.cc
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-class Solution {
+/*class Solution {
     unordered_map<char, int> count(const string &word) {
         unordered_map<char, int> counter;
         for (const char &c : word) {
@@ -36,6 +36,38 @@ public:
             if ((word.length() < ans.length() || ans.length() == 0) && check(counter, word)) ans = word;
         }
         return ans;
+    }
+};*/
+
+class Solution {
+public:
+    string shortestCompletingWord(string licensePlate, vector<string> &words) {
+        sort(words.begin(), words.end(), [](const string &lhs, const string &rhs) {
+            return lhs.length() == rhs.length() ? false : lhs.length() <= rhs.length();
+        });
+        vector<int> cnt(26, 0);
+        covert(cnt, licensePlate);
+        for (const string &word: words) {
+            vector<int> tmp(26, 0);
+            covert(tmp, word);
+            if (check(cnt, tmp)) return word;
+        }
+        return "";
+    }
+
+private:
+    bool check(const vector<int> &lhs, const vector<int> &rhs) {
+        // lhs 代表 licensePlate 里面的单词计数
+        for (int idx = 0; idx < 26; ++idx) if (lhs[idx] > rhs[idx]) return false;
+        return true;
+    }
+
+    void covert(vector<int> &container, const string &word) {
+        if (container.size() < 26) return;
+        for (char c: word) {
+            if (c >= 'a' && c <= 'z') container[c - 'a']++;
+            else if (c >= 'A' && c <= 'Z') container[c - 'A']++;
+        }
     }
 };
 
