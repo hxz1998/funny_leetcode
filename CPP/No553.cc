853a7ea386aeedafda4721bacbdc8ed460ea736f
Monkey <1466947023@qq.com>
2022-02-27 23:16:30 +0800

    同步源码

--- CPP/No553.cc
+++ CPP/No553.cc
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Solution {
+/*class Solution {
 public:
     string optimalDivision(vector<int> &nums) {
         // 只要保证除数最小，被除数最大就好
@@ -22,6 +22,24 @@ public:
             if (i == 0) res += "(";
         }
         return res += ")";
+    }
+};*/
+
+class Solution {
+public:
+    string optimalDivision(vector<int> &nums) {
+        // 只要保证除数最小，被除数最大就好
+        string ret = "";
+        if (nums.size() == 0) return ret;
+        else if (nums.size() == 1) return to_string(nums[0]);
+        else if (nums.size() == 2) return to_string(nums[0]) + "/" + to_string(nums[1]);
+        for (size_t idx = 0; idx < nums.size(); ++idx) {
+            ret.append(to_string(nums[idx]));
+            if (idx != nums.size() - 1) ret.push_back('/');
+            if (idx == 0) ret.push_back('(');
+        }
+        ret.push_back(')');
+        return ret;
     }
 };
 
